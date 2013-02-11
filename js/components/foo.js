30005ab97c9aed21150dc5918e065a61b79a064e
Valentino Aluigi <valentino.aluigi@rga.com>
2013-02-11 17:51:57 +0000

    Forgot to add a couple of ifles

---
+++ js/components/foo.js
@@ -0,0 +1,22 @@
+tks.component('sortable-table', function(core){
+  
+  // we could even remove this and use it as a default
+  core.register('sortable-table', '[data-component=sortable-table]');
+
+  core.on('ready', function(){
+    // initialization required on dom ready
+    // for all the plugins
+  });
+
+  return function CarouselComponent(){
+
+    this.init = function(){
+
+    }
+
+    this.next = function(){
+
+    }
+  };
+
+});
